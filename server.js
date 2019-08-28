c506731de0c185e408909fb0d01ace3de003c63b
unknown <alfonso.pulido8015@gmail.com>
2019-08-28 17:08:49 -0500

    server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/plataformaprendizaje'));
+app.use(express.static('./dist/app-aprendizaje'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/plataformaprendizaje/index.html'));
+res.sendFile(path.join(__dirname,'/dist/app-aprendizaje/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
